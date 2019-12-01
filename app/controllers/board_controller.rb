3c4d456201ff4df4c40579e9c89d3a76466bc452
Shutarou Takayama <hi.mountain110916@gmail.com>
2019-12-01 21:53:35 +0900

    投稿フォーム

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,4 +2,12 @@ class BoardController < ApplicationController
   def index
     @boards = Board.all
   end
+  def show
+    @board = Board.find(board)
+    @comments = Comment.where(board_id: params[:id])
+  end
+  def create
+    Board.create(:title => params[:title])
+    redirect_to index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'board#index'
+  get '/', to: 'board#index', as: :index
+  post '/', to: 'board#create'
 
   get '/test', to: 'application#test'
   get '/visualize', to: 'application#visualize'
