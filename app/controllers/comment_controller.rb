60a004f82865440428a57d1f95c45c136d3489ba
Shutarou Takayama <hi.mountain110916@gmail.com>
2019-12-02 02:16:00 +0900

    コメント数decrementを忘れていたので追加

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -2,6 +2,7 @@ class CommentController < ApplicationController
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy()
+    Board.decrement_counter(:comments_count, params[:board_id], touch:true)
     redirect_to show_path(params[:board_id])
   end
 end
